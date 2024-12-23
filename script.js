1a787728facd62eca56f2912fdd9ec6ec77753dd
Efraim Crs <efraimcrsilva@gmail.com>
2024-12-23 11:07:01 -0300

    Aatualizacao

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const loginForm = document.getElementById('loginForm');
 // 2 add o evento de envio
 loginForm.addEventListener('submit', function (e) {e.preventDefault(); //evita o carregamento da pagina
 
-//capt. os valores dos campos
+// 3 capt. os valores dos campos
 const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
 
-// Simulação de autenticação
+// 4 Simulação de autenticação
 if (email === 'admin@example.com' && password === '123456') {
     alert('Login bem-sucedido!');
-    // Redirecionar para outra página (se necessário)
-    // window.location.href = 'dashboard.html';
+    // 5 Redirecionar para outra página (se necessário)
+    // 6 window.location.href = 'dashboard.html';
   } else {
-    // Exibe mensagem de erro
+    // 7 Exibe mensagem de erro
     alert('Email ou senha incorretos!');
-    // Adiciona borda vermelha aos campos
+    // 8 Adiciona borda vermelha aos campos
     document.getElementById('email').classList.add('is-invalid');
     document.getElementById('password').classList.add('is-invalid');
   }
